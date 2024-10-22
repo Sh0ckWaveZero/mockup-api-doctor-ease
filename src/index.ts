3ea803d4cf074a7c28e6d6556f52bf027bca377c
Aphidet-Hongseetong <ottooffple@icloud.com>
2024-10-22 15:54:20 +0700

    Add Dockerfile and docker-compose.yml for containerization

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,27 @@
+import cors from '@elysiajs/cors';
 import { Elysia } from "elysia";
+import { DefaultContext, rateLimit } from 'elysia-rate-limit';
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia({
+  prefix: "/api/v1",
+})
+  .trace(async ({ onHandle }) => {
+    onHandle(({ begin, onStop }) => {
+      onStop(({ end }) => {
+        console.log('🪝 handle took', end - begin, 'ms')
+      })
+    })
+  })
+  .use(cors())
+  .use(rateLimit({
+    // define max cache size to 1,000
+    context: new DefaultContext(1_000),
+  }))
+  .get("/", () => "Hello developer! 🥳")
+  .post("/packages", ({ body }) => {
+    return { message: "Packages endpoint", data: body };
+  })
+  .listen(6301);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
